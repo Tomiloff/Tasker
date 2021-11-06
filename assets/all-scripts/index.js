e80f3f5edd8375541595d899a12e787c68f02ee2
Vadim <Tomilov_Vadim@list.ru>
2021-11-06 17:12:06 +0300

    js for ".header-btn"

--- assets/all-scripts/index.js
+++ assets/all-scripts/index.js
@@ -4,4 +4,11 @@ $(function () {
       $(".header-list").slideToggle();
    })
 
+   $(document).click(function (e) {
+      let topAccordion = $(".header-list, .header-btn");
+      if (!topAccordion.is(e.target) && topAccordion.has(e.target).length == 0) {
+         $(".header-list").slideUp();
+      }
+   });
+
 });
