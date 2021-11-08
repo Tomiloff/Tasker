348a1e7e6416db601b17a88bebfe77c0fde1d39e
Vadim <Tomilov_Vadim@list.ru>
2021-11-09 00:37:25 +0300

    Added a mobile menu

--- assets/all-scripts/index.js
+++ assets/all-scripts/index.js
@@ -1,14 +1,25 @@
 $(function () {
-
-   $(".header-btn-icon").click(function () {
+   // .header-btn-icon - кнопка меню в хедере
+   $("#header-btn").click(function () {
       $(".header-list").slideToggle();
    })
 
+   // Обработка клика вне элемента аккордиона .header-list
    $(document).click(function (e) {
       let topAccordion = $(".header-list, .header-btn");
       if (!topAccordion.is(e.target) && topAccordion.has(e.target).length == 0) {
          $(".header-list").slideUp();
       }
+   });
+
+
+   // Обработка клика по кнопке мобильного меню #header-btn-mobile
+   $("#header-btn-mobile").click(function () {
+      $(".mobile-menu").slideToggle();
+   });
+
+   $(".mobile-menu-exit").click(function () {
+      $(".mobile-menu").slideUp();
    });
 
 });
